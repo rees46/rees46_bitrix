7316c741c3c191346f8344e8dec21fa8a8bb55dd
Anton Smirnov <anton.smirnov@mkechinov.ru>
2013-12-19 19:07:04 +0400

    return back after install/uninstall

--- install/step.php
+++ install/step.php
@@ -5,3 +5,9 @@ if (!check_bitrix_sessid()) {
 }
 
 CAdminMessage::ShowNote('Модуль REES46 установлен');
+
+?>
+<form action="<?= $APPLICATION->GetCurPage() ?>">
+    <input type="hidden" name="lang" value="<?= LANG ?>">
+    <input type="submit" name="" value="<?= GetMessage("MOD_BACK") ?>">
+<form>
--- install/unstep.php
+++ install/unstep.php
@@ -5,3 +5,9 @@ if (!check_bitrix_sessid()) {
 }
 
 CAdminMessage::ShowNote('Модуль успешно удален из системы');
+
+?>
+<form action="<?= $APPLICATION->GetCurPage() ?>">
+  <input type="hidden" name="lang" value="<?= LANG ?>">
+  <input type="submit" name="" value="<?= GetMessage("MOD_BACK") ?>">
+<form>
