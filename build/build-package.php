a3fa71e4709851d1fd644936599af01341e3bc08
Anton Smirnov <anton.smirnov@mkechinov.ru>
2014-01-16 17:46:10 +0400

    create 1.0.2 * recommender fixes * build script update

--- build/build-package.php
+++ build/build-package.php
@@ -8,6 +8,11 @@
 define('MODULE_DIR', dirname(__DIR__));
 define('BUILD_DIR', __DIR__ . '/rees46recommender');
 
+class CModule {} // we do not require bitrix things but rees46recommender is inherited from CModule
+require_once __DIR__ .'/../install/index.php'; // get rees46recommender data
+
+$version = (new rees46recommender())->MODULE_VERSION;
+
 function run($path, $command)
 {
 	print `cd $path && $command`;
@@ -26,6 +31,6 @@ run (__DIR__, 'cp '. MODULE_DIR .'/options.php '. BUILD_DIR);
 
 print "Creating archive...\n";
 
-run (__DIR__, 'rm rees46recommender.zip');
-run (__DIR__, 'zip -r rees46recommender.zip rees46recommender');
+run (__DIR__, "rm rees46recommender-{$version}.zip");
+run (__DIR__, "zip -r rees46recommender-{$version}.zip rees46recommender");
 run (__DIR__, 'rm -rf rees46recommender');
