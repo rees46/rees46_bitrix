0c52971271f16ad41da7a29476fd264791896807
Anton Smirnov <anton.smirnov@mkechinov.ru>
2014-01-27 13:47:19 +0400

    meleon hack — check if modules are installed

--- meleon/include/order-redirect.php
+++ meleon/include/order-redirect.php
@@ -2,10 +2,11 @@
 
 require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php';
 
-CModule::IncludeModule('sale');
-CModule::IncludeModule('rees46recommender');
+if (IsModuleInstalled('rees46recommender') && IsModuleInstalled('sale')) {
+	CModule::IncludeModule('sale');
+	CModule::IncludeModule('rees46recommender');
 
-Rees46Func::purchase(null);
-Rees46Func::includeJs();
+	Rees46Func::purchase(null);
+}
 
 LocalRedirect(GetOrderUrl());
