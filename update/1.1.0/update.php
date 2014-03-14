6d7cc885e9c4f42d637dec46e9b463197e5cb929
Anton Smirnov <anton.smirnov@mkechinov.ru>
2014-03-14 18:03:07 +0400

    updater — classes only

--- update/1.1.0/update.php
+++ update/1.1.0/update.php
@@ -1,5 +1,3 @@
 <?php
 
 $updater->CopyFiles('classes', 'modules/mk.rees46/classes');
-$updater->CopyFiles('install/components/rees46', 'components/rees46');
-$updater->CopyFiles('install/include', '../include');
