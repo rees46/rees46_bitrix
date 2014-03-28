b67dcfa4f53b5c823fa8a77d39f3b99391973ff1
Anton Smirnov <anton.smirnov@mkechinov.ru>
2014-03-28 15:59:43 +0400

    * no moar dependences so no need to update them * due to heavy changes new version should be 1.2 * composer.json is also no longer required * some fixes in the build script

--- update/1.2.0/update.php
+++ update/1.2.0/update.php
@@ -1,5 +1,8 @@
 <?php
 
+$updater->CopyFiles('install/components/rees46', 'components/rees46');
+$updater->CopyFiles('install/include', '../include');
+
 UnRegisterModuleDependences('sale', 'OnBasketAdd',          'mk.rees46', 'Rees46Func', 'cart');
 UnRegisterModuleDependences('sale', 'OnBeforeBasketDelete', 'mk.rees46', 'Rees46Func', 'removeFromCart');
 UnRegisterModuleDependences('sale', 'OnBasketOrder',        'mk.rees46', 'Rees46Func', 'purchase');
