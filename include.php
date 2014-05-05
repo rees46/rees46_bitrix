8cd9f67cb49a8bf7e8727f545d5b9381f070f284
Anton Smirnov <anton.smirnov@mkechinov.ru>
2014-05-05 13:53:13 +0400

    replace bitrix autoload with composer's classloader

--- include.php
+++ include.php
@@ -5,14 +5,21 @@ if (class_exists('mk_rees46') === false) {
 	require __DIR__ . '/install/index.php';
 }
 
-CModule::AddAutoloadClasses(
-	mk_rees46::MODULE_ID,
-	array(
-		'Rees46\\Functions' => 'classes/Rees46/Functions.php',
-		'Rees46\\Component\\RecommendHandler' => 'classes/Rees46/Component/RecommendHandler.php',
-		'Rees46\\Component\\RecommendRenderer' => 'classes/Rees46/Component/RecommendRenderer.php',
-		'Rees46\\Includes\\Controller' => 'classes/Rees46/Includes/Controller.php',
+if (class_exists('Composer\\Autoload\\ClassLoader') === false) {
+	require __DIR__ . '/classes/Composer/Autoload/ClassLoader.php';
+}
+
+// unobstructively add autoloader
+if (function_exists('__autoload') && ( // if we have an old autoload func
+		is_array(spl_autoload_functions()) === false || // and if autoload stack is not initialized or
+		in_array('__autoload', spl_autoload_functions()) === false // if it lacks __autoload
 	)
-);
+) {
+	spl_autoload_register('__autoload'); // register old autoload
+}
+
+$loader = new \Composer\Autoload\ClassLoader();
+$loader->add('Rees46\\', __DIR__ . '/classes/');
+$loader->register(true);
 
 \Rees46\Functions::showRecommenderCSS();
