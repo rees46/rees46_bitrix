980fbd4bb8131bce33bb5c16f5ca86606824408f
Nick0009 <al@it-studio.ru>
2016-06-02 21:40:53 +0300

    fix for new Bitrix

--- classes/Rees46/Events.php
+++ classes/Rees46/Events.php
@@ -6,6 +6,53 @@ use Rees46\Bitrix\Data;
 
 class Events
 {
+	
+	 //add order
+	 public static function OnSaleOrderSavedHandler(\Bitrix\Main\Event $event)
+	{
+		
+		$parameters = $event->getParameters();
+		$order = $parameters['ENTITY'];
+		$order_id = $order->getId();	
+		
+		$items = array();
+
+		foreach (Data::getOrderItems($order_id) as $item) {
+			$items []= array(
+				'item_id' => $item['PRODUCT_ID'],
+				'amount'  => $item['QUANTITY'],
+				'price' => $item['PRICE']
+			);
+		}
+
+		Functions::cookiePushPurchase($items, $order_id);
+
+	}
+	
+	 //dell product basket
+	public static function  OnBasketDeleteMy(\Bitrix\Main\Event $event)
+	{
+		$parameters = $event->getParameters();
+		$values = $parameters['VALUES'];
+		$basket_id = $values['PRODUCT_ID'];
+		$item = Data::getItemArray($basket_id);
+		Functions::cookiePushData('remove_from_cart', $item);		
+	}
+	
+	 //add product basket
+	public static function  OnSaleBasketItemBeforeSavedMy(\Bitrix\Main\Event $event)
+	{
+		
+		$parameters = $event->getParameters();
+		$order = $parameters['ENTITY'];
+		//$basket_id = $order->getId();
+		//$item = Data::getBasketArray($basket_id);
+		$basket_id = $order->getField('PRODUCT_ID');
+		$item = Data::getItemArray($basket_id);
+		Functions::cookiePushData('cart', $item);
+		
+	}
+	
 	/**
 	 * push view event
 	 *
