3b3d598fb96aaeba4f1b5b621f4bbf465074a20e
Andrey Veprikov <av@rees46.com>
2019-01-09 13:08:44 +0300

    v3.0.3 updater
    v3.0.3 updater

--- update/3.0.3/updater.php
+++ update/3.0.3/updater.php
@@ -1,4 +1,19 @@
 <?php
+DeleteDirFilesEx('/include/rees46-recommender.php');
+DeleteDirFilesEx('/bitrix/modules/mk.rees46/update.php');
+DeleteDirFilesEx('/bitrix/modules/mk.rees46/classes/Pest.php');
+DeleteDirFilesEx('/bitrix/modules/mk.rees46/classes/REES46.php');
+DeleteDirFilesEx('/bitrix/modules/mk.rees46/classes/Rees46Func.php');
+DeleteDirFilesEx('/bitrix/modules/mk.rees46/classes/Rees46IncludeRunner.php');
+DeleteDirFilesEx('/bitrix/modules/mk.rees46/classes/.DS_Store');
+DeleteDirFilesEx('/bitrix/modules/mk.rees46/classes/Rees46/Bitrix/.DS_Store');
+DeleteDirFilesEx('/bitrix/modules/mk.rees46/classes/Rees46/Component/.DS_Store');
+DeleteDirFilesEx('/bitrix/modules/mk.rees46/classes/Rees46/Service/.DS_Store');
+DeleteDirFilesEx('/bitrix/modules/mk.rees46/classes/Rees46/.DS_Store');
+DeleteDirFilesEx('/bitrix/modules/mk.rees46/install/.DS_Store');
+DeleteDirFilesEx('/bitrix/modules/mk.rees46/install/components/rees46/recommend/templates/.default/style.css');
+DeleteDirFilesEx('/bitrix/modules/mk.rees46/install/include/rees46-recommender.php');
+DeleteDirFilesEx('/bitrix/modules/mk.rees46/lang/ru/classes/Rees46IncludeRunner.php');
 
 $updater->CopyFiles('install/components/rees46', 'components/rees46');
 $updater->CopyFiles('install/include', '../include');
