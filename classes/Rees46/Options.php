5829e5ca0c37acbda0d1f8d5ed04a891c1c8bbee
svs <svs@rees46.com>
2022-09-15 10:02:30 +0300

    Update Options

--- classes/Rees46/Options.php
+++ classes/Rees46/Options.php
@@ -1,23 +1,35 @@
 <?php
-
-namespace Rees46;
-
-class Options
-{
-
-	public static function getShopID()
+	
+	namespace Rees46;
+	
+	use Bitrix\Main\Config\Option;
+	
+	class Options
 	{
-		return \COption::GetOptionString(\mk_rees46::MODULE_ID, 'shop_id');
+		
+		public static function getShopID()
+		{
+			return Option::get(\mk_rees46::MODULE_ID, 'shop_id');
+		}
+		
+		public static function getShopSecret()
+		{
+			return Option::get(\mk_rees46::MODULE_ID, 'shop_secret');
+		}
+		
+		public static function getStream()
+		{
+			return Option::get(\mk_rees46::MODULE_ID, 'stream');
+		}
+		
+		public static function getUserGroups()
+		{
+			return explode(',', Option::get(\mk_rees46::MODULE_ID, 'user_groups'));
+		}
+		
+		public static function getInstantSearchEmbedded()
+		{
+			return Option::get(\mk_rees46::MODULE_ID, 'instant_search_embedded', \mk_rees46::INSTANT_SEARCH_DEFAULT) ? true : false;
+		}
+		
 	}
-
-	public static function getShopSecret()
-	{
-		return \COption::GetOptionString(\mk_rees46::MODULE_ID, 'shop_secret');
-	}
-
-	public static function getInstantSearchEmbedded()
-	{
-		return \COption::GetOptionInt(\mk_rees46::MODULE_ID, 'instant_search_embedded', \mk_rees46::INSTANT_SEARCH_DEFAULT) ? true : false;
-	}
-
-}
